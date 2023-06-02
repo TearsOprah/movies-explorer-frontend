c7d435c030d95e6ef8cd747765dcd9172e270d7a
TearsOprah <qmeshokp@mail.ru>
2023-06-02 13:46:27 +0300

    feat: верстка NotFound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,14 @@
 import './NotFound.css'
+import {Link} from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <>
-      <h1>NotFound</h1>
-    </>
+    <div className={'not-found'}>
+      <div className={'not-found__block'}>
+        <p className={'not-found__error'}>404</p>
+        <p className={'not-found__message'}>Страница не найдена</p>
+      </div>
+      <Link className={'not-found__link'} to={'/'}>Назад</Link>
+    </div>
   )
 }
