1b6cb00bec82a73de8d81504f6d812f1f31e9035
TearsOprah <qmeshokp@mail.ru>
2023-06-02 16:59:57 +0300

    feat: дописал логику работы header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import './Header.css'
 import logoIcon from '../../images/logo.svg'
 import burgerIcon from '../../images/burger.svg'
 import profileIcon from '../../images/profile_icon.svg'
+import closerIcon from '../../images/closer.svg'
 import {Link} from "react-router-dom";
 import {useState, useEffect} from "react";
 
@@ -42,19 +43,26 @@ export default function Header() {
       <nav className={'navigation'}>
 
         <button className={'header__burger'} onClick={handleMenuClick}>
-          <img src={burgerIcon} />
+          <img src={isOpenMenu ? closerIcon : burgerIcon} />
         </button>
 
         {isOpenMenu && (
-          <ul className={`navigation__list ${isOpenMenu ? 'navigation__list-open' : ''}`}>
-            {navLink.map((link, index) => (
-              <li key={index} className={`navigation__item ${activeLink === link.path ? "navigation__item-active" : ""}`}>
-                <Link to={link.path} className={'navigation__link'} onClick={() => {}}>
-                  {link.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <>
+            <ul className={`navigation__list ${isOpenMenu ? 'navigation__list-open' : ''}`}>
+              {navLink.map((link, index) => (
+                <li key={index} className={`navigation__item ${activeLink === link.path ? "navigation__item-active" : ""}`}>
+                  <Link to={link.path} className={'navigation__link'} onClick={() => {}}>
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+
+            <Link className={'profile-button'} to={'/'}>
+              <img className={'profile-button__icon'} src={profileIcon}/>
+              <p>Аккаунт</p>
+            </Link>
+          </>
         )}
 
         {!isOpenMenu && (
@@ -71,8 +79,7 @@ export default function Header() {
 
       </nav>
 
-
-      <Link className={'profile-button'} to={'/'}>
+      <Link className={'profile-button profile-button_hidden'} to={'/'}>
         <img className={'profile-button__icon'} src={profileIcon}/>
         <p>Аккаунт</p>
       </Link>
