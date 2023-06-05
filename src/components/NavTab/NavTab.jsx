77c64247fc81ef4f0cc28175b15908868f55b3c9
TearsOprah <qmeshokp@mail.ru>
2023-06-05 19:54:43 +0300

    feat: верстка NavTab

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -2,7 +2,8 @@ import './NavTab.css'
 export default function NavTab() {
   return (
     <nav className={'landing-navigation'}>
-
+      <button className={'landing-navigation__button register-button'}>Регистрация</button>
+      <button className={'landing-navigation__button login-button'}>Войти</button>
     </nav>
   )
 }
