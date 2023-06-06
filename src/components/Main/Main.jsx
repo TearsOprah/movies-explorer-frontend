7d14d82463e09691fa3086e3ebff39b2be7e2e3a
TearsOprah <qmeshokp@mail.ru>
2023-06-06 13:51:22 +0300

    feat: верстка Techs

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import './Main.css'
 import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
 
 export default function Main() {
 
@@ -8,6 +9,7 @@ export default function Main() {
     <main className={'main'}>
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   )
 }
