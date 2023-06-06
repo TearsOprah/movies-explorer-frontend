5c48d3d23d49d2cea8b4b08a0173e54960910c93
TearsOprah <qmeshokp@mail.ru>
2023-06-06 16:30:19 +0300

    feat: верстка Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,19 @@
 import './Footer.css'
 export default function Footer() {
   return (
-    <></>
+    <footer className={'footer'}>
+      <p className={'footer__description'}>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className={'footer__container'}>
+        <p className={'footer__copyright'}>© 2023</p>
+        <ul className={'footer__links'}>
+          <li>
+            <a className={'footer__link'}>Яндекс.Практикум</a>
+          </li>
+          <li>
+            <a className={'footer__link'}>Github</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   )
 }
