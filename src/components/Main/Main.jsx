fdcad7c13ca1d15728fbdfa0c8f88b798e9ef87d
TearsOprah <qmeshokp@mail.ru>
2023-06-06 15:53:52 +0300

    feat: верстка Portfolio

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import Promo from "../Promo/Promo";
 import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import AboutMe from "../AboutMe/AboutMe";
+import Portfolio from "../Portfolio/Portfolio";
 
 export default function Main() {
 
@@ -12,6 +13,7 @@ export default function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Portfolio />
     </main>
   )
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,29 @@
 import './Portfolio.css'
+import linkIcon from '../../images/link.svg'
 export default function Portfolio() {
   return (
-    <></>
+    <section className={'main-section portfolio'}>
+      <h2 className={'portfolio__title'}>Портфолио</h2>
+      <ul className={'portfolio__list'}>
+        <li className={'portfolio__item'}>
+          <a className={'portfolio__link'} href={''}>
+            <h3 className={'portfolio__item-name'}>Статичный сайт</h3>
+            <img className={'portfolio__link-icon'} src={linkIcon} alt={'link-icon'} />
+          </a>
+        </li>
+        <li className={'portfolio__item'}>
+          <a className={'portfolio__link'} href={''}>
+            <h3 className={'portfolio__item-name'}>Адаптивный сайт</h3>
+            <img className={'portfolio__link-icon'} src={linkIcon} alt={'link-icon'} />
+          </a>
+        </li>
+        <li className={'portfolio__item'}>
+          <a className={'portfolio__link'} href={''}>
+            <h3 className={'portfolio__item-name'}>Одностраничное приложение</h3>
+            <img className={'portfolio__link-icon'} src={linkIcon} alt={'link-icon'} />
+          </a>
+        </li>
+      </ul>
+    </section>
   )
 }
