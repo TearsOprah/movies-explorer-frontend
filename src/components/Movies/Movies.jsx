b88caa102e2842446afd33710b1a1825fab543cd
TearsOprah <qmeshokp@mail.ru>
2023-06-16 20:03:29 +0300

    feat: сохранение состояния toggle и поиска в movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,4 +1,5 @@
 import './Movies.css';
+import { useEffect } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
@@ -16,6 +17,33 @@ export default function Movies({ allMovies, errorFetchAllMovies, savedMovies, ha
     handleSearch,
     handleToggleShortFilmOnly,
   } = useMoviesSearch(allMovies);
+
+  // Чтение сохраненных данных из локального хранилища при монтировании компонента
+  useEffect(() => {
+    const savedSearchQuery = localStorage.getItem('searchQuery');
+    const savedShortFilmOnly = localStorage.getItem('shortFilmOnly');
+
+    if (savedSearchQuery && searchQuery !== savedSearchQuery) {
+      setSearchQuery(savedSearchQuery);
+    }
+
+    if (savedShortFilmOnly && shortFilmOnly !== (savedShortFilmOnly === 'true')) {
+      handleToggleShortFilmOnly(savedShortFilmOnly === 'true');
+    }
+  }, []);
+
+  // Сохранение данных в локальное хранилище при изменении searchQuery или shortFilmOnly
+  useEffect(() => {
+    if (searchQuery !== localStorage.getItem('searchQuery')) {
+      localStorage.setItem('searchQuery', searchQuery);
+    }
+  }, [searchQuery]);
+
+  useEffect(() => {
+    if (shortFilmOnly !== (localStorage.getItem('shortFilmOnly') === 'true')) {
+      localStorage.setItem('shortFilmOnly', shortFilmOnly.toString());
+    }
+  }, [shortFilmOnly]);
 
   return (
     <main className={'movies'}>
